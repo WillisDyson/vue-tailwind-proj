cb2100aa78c93960a46f6cdd5d45c54e886c15b8
Willis Dyson <willisdyson@gmail.comD>
2025-07-28 11:43:11 +0100

    Adding button hover styling

--- vue-tailwind-app/src/components/UrlFormSubmit.vue
+++ vue-tailwind-app/src/components/UrlFormSubmit.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <button type="submit" class="rounded-full hover:cursor-pointer bg-[#03874b] px-10 py-2 text-[1.6rem]">
+  <button type="submit" class="rounded-full hover:cursor-pointer bg-[#03874b] hover:bg-[#006839] px-10 py-2 text-[1.6rem] duration-300">
   Submit
   </button>
 </template>
